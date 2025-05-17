30dc78159d5e893c4e78e6793046063f1bf9ffae
EmaaZmaker <emaazbinammar@gmail.com>
2025-05-17 09:39:46 +0400

    huh

---
+++ apy.py
@@ -0,0 +1,6 @@
+import math
+def circumfrence (Radius):
+    return 2*math.pi*Radius
+num1=float(input("enter the Radius"))
+print("a circumfrence or diameter of your circle is",circumfrence(num1))
+#mam i only searched the formula, i did not copy anything. i just implemented it
